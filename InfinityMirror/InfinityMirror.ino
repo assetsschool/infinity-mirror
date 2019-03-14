df273c49477a30c3ce559fe58597d4b77ad31a27
Iain Moncrief <Iainmoncrief@gmail.com>
2019-03-13 15:42:28 -1000

    messing arround with colors

--- InfinityMirror/InfinityMirror.ino
+++ InfinityMirror/InfinityMirror.ino
@@ -9,15 +9,17 @@
 #include <Adafruit_DotStar.h>
 #include <SPI.h>
 
-#define NUMPIXELS 144
-#define DATAPIN   6
-#define CLOCKPIN  5
+
+// Compile time definitions
+#define NUMPIXELS 144 // Pixel count
+#define DATAPIN 6 // Green wire
+#define CLOCKPIN 5 // Blue wire
+#define ANALOGPOTPIN 2 // Potentiometer pin
+#define COLORSTEP 8 // How much the color changes
 
 Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
-const int analogPotPin = 2;
-
-int head  = 0;
+int head = 0;
 int tail = -143;
 
 uint32_t color = 0xFF0000; // Color (starts red)
@@ -27,36 +29,49 @@ int pot = 0; // Potentiometer variable
 
 void setup() {
 
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
     clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
-  #endif
+#endif
 
-  strip.begin();
-  strip.show(); // Clears the strip
+    strip.begin();
+    strip.show(); // Clears the strip
 }
 
 void loop() {
 
-  strip.setPixelColor(head, color); // Sets head pixel to color
-  strip.setPixelColor(tail, 0); // Sets tail pixel to OFF
-  strip.show();  // Update strip
+    strip.setPixelColor(head, color); // Sets head pixel to color
+    strip.setPixelColor(tail, 0);     // Sets tail pixel to OFF
+    strip.show();                     // Update strip
 
-  pot = analogRead(analogPotPin); // Reads the potentiometer
+    pot = analogRead(ANALOGPOTPIN); // Reads the potentiometer
 
-  pot = map(pot, 0, 1023, 0, 100); // Maps the reading to (0 - 100)
-  delay(pot); // Delays for that many miliseconds
+    pot = map(pot, 0, 1023, 0, 100); // Maps the reading to (0 - 100)
+    delay(pot); // Delays for that many miliseconds
 
-  if (++head >= NUMPIXELS) {
-    
-    head = 0;
-    
-    if ((color >>= 8) == 0) {
-      color = 0xFF0000;
+    if (++head >= NUMPIXELS) {
+
+        head = 0;
+
+        if ((color >>= COLORSTEP) == 0) {
+            color = 0xFF0000;
+        }
     }
-  }
 
-  // Resets the head position to 0
-  if (++tail >= NUMPIXELS) {
-    tail = 0;
-  }
+    // Resets the head position to 0
+    if (++tail >= NUMPIXELS) {
+        tail = 0;
+    }
 }
+
+
+// TEST AND IMPLEMENT ME
+
+// const int step = 8; // Select this to be a power of two if you want the maximum brightness to be reachable
+// for( int red = 0x00; red <= 0xFF; red += step ) {
+//     for( int green = 0x00; green <= 0xFF; green += step ) {
+//         for( int blue = 0x00; blue <= 0xFF; blue += step ) {
+//             const int color = blue << 16 + green << 8 + red;
+//             // Change the led settings here.
+//         }
+//     }
+// } 
